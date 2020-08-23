c1a2e49158777b3ef833f8777d0d04724421968a
Herbrand Hofker <herbrand.hofker@gmail.com>
2020-08-23 21:37:15 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import * as AdaptiveCards from "adaptivecards";
 require('./mystyles.scss');
+
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+
+const el = document.getElementById("carousel");
+for (var i = 21; i < 99; i++) {
+    const div = document.createElement("div");
+    div.classList.add("card-image");
+    div.style = "padding: 20px; width: 700px; margin: auto;";
+    const figure = document.createElement("figure");
+    figure.classList.add("image");
+    figure.classList.add("is-256x256");
+    const img = document.createElement("img");
+    img.src = "./images/" + i + ".jpg";
+    figure.append(img);
+    div.append(figure);
+    el.append(div);
+}
+
+bulmaCarousel.attach('#carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 1
+});
